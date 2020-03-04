51a7590aec65e1ca5b5b99911f28c07174065305
JoseBi4Group <jm.roman@bi4group.com>
2020-03-04 10:38:59 +0100

    enable CORS with flask cors

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from database.db import initialize_db
 from resources.movie import movies
 from resources.user import users
 
 app = Flask(__name__)
+CORS(app)
 bcrypt = Bcrypt(app)
 
 #app.config.from_envvar('ENV_FILE_LOCATION')
